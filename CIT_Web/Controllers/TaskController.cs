6950173ff7ba868a679225ae5f1c8ea49907b9f9
hariom022 <hariommaurya022@gmail.com>
2024-09-13 18:49:18 +0530

    the task view module has been created and layout.cshtml updated

---
+++ CIT_Web/Controllers/TaskController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CIT_Web.Controllers
+{
+    public class TaskController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GroupData()
+        {
+            return View();
+        }
+    }
+}
