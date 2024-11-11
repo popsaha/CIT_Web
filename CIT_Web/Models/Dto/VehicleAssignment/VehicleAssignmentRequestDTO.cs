cfc41c19e2f69d7d67980fb9c9586151d7a684b3
hariom022 <hariommaurya022@gmail.com>
2024-11-11 16:03:24 +0530

    vehicleAssignmentDate  is removed from VehicleAssignmentRequestDTO Model

--- CIT_Web/Models/Dto/VehicleAssignment/VehicleAssignmentRequestDTO.cs
+++ CIT_Web/Models/Dto/VehicleAssignment/VehicleAssignmentRequestDTO.cs
@@ -5,6 +5,6 @@
         public int? LeadID { get; set; }
         public int? ChaseID { get; set; }
         public int? CrewCommanderID { get; set; }
-        public string? VehicleAssignDate { get; set; }
+       
     }
 }
