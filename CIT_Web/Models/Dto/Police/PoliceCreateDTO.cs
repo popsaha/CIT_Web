87d201a874650c754575192411d087d37a7e32ef
hariom022 <hariommaurya022@gmail.com>
2024-12-31 18:37:14 +0530

    validation applied on police module

--- CIT_Web/Models/Dto/Police/PoliceCreateDTO.cs
+++ CIT_Web/Models/Dto/Police/PoliceCreateDTO.cs
@@ -1,8 +1,12 @@
-﻿namespace CIT_Web.Models.Dto.Police
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CIT_Web.Models.Dto.Police
 {
     public class PoliceCreateDTO
     {
         public string Name { get; set; }
+        [Required(ErrorMessage = "Contact Number is required.")]
+        [RegularExpression(@"^\d+$", ErrorMessage = "Contact Number must be numeric.")]
         public string ContactNumber { get; set; }
         public string Address { get; set; }
        
